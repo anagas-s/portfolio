3ada666eb850a6d1c5270ed09001ccb1df29e720
Anagas <anagassatheeshkumar@gmail.com>
2024-08-15 22:08:19 +0530

    update

--- src/components/WorkHistory.tsx
+++ src/components/WorkHistory.tsx
@@ -10,36 +10,7 @@ import {
 } from "@tabler/icons-react";
 
 export const WorkHistory = () => {
-  return (
-    <div>
-      {timeline.map((item, index) => (
-        <div
-          className="flex md:flex-row flex-col space-y-10 md:space-y-0 space-x-10 my-20 relative"
-          key={`timeline-${index}`}
-        >
-          <Paragraph className="w-40">{item.date}</Paragraph>
-          <div>
-            <Heading
-              as="h5"
-              className="text-lg md:text-lg lg:text-lg text-emerald-500"
-            >
-              {item.company}
-            </Heading>
-            <Paragraph className="text-base md:text-base lg:text-base font-semibold">
-              {item.title}
-            </Paragraph>
-            <Paragraph className="text-sm md:text-sm lg:text-sm mb-4">
-              {item.description}
-            </Paragraph>
-
-            {item.responsibilities.map((responsibility, index) => (
-              <Step key={responsibility}>{responsibility}</Step>
-            ))}
-          </div>
-        </div>
-      ))}
-    </div>
-  );
+  return <div></div>;
 };
 
 const Step = ({ children }: { children: React.ReactNode }) => {
